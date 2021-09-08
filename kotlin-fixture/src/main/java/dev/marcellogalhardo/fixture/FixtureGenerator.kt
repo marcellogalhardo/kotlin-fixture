c94b47d2ce4bd1709eb9c8e84011c438252112d1
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-09-08 21:42:38 +0200

    Improve generator allowing easy copying for data classes

--- kotlin-fixture/src/main/java/dev/marcellogalhardo/fixture/FixtureGenerator.kt
+++ kotlin-fixture/src/main/java/dev/marcellogalhardo/fixture/FixtureGenerator.kt
@@ -5,5 +5,8 @@ class FixtureGenerator<T> internal constructor(
     private val generate: Fixture.() -> T
 ) {
 
-    fun generate(): T = generate(fixture)
+    fun generate(block: (Fixture.(T) -> T)? = null): T {
+        val generated = generate(fixture)
+        return if (block == null) generated else block(fixture, generated)
+    }
 }
